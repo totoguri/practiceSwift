49bb0de88aacfb3350d24bd4ebc619a3b7c85587
totoguri <finalyo4@naver.com>
2021-09-14 16:59:16 +0900

    swift

--- main.swift
+++ main.swift
@@ -1,59 +1,83 @@
+// class Person {
+//     var name: String = ""
+//     var age: Int = 0
+
+//     var introduction: String {
+//         return "Name : \(name). Age : \(age)"
+//     }
+
+//     func speak() {
+//         print("GaNaDa")
+//     }
+
+//     class func introduceClass() -> String {
+//         return "Hope"
+//     }
+// }
+
+// class Student: Person {
+//     var grade: String = "F"
+
+//     func study() {
+//         print("Study hard...")
+//     }
+
+//     override func speak() {
+//         print("I'm a student.")
+//     }
+// }
+
+// class UniversityStudent: Student {
+//     var major: String = ""
+
+//     class func introduceClass() {
+//         print(super.introduceClass())
+//     }
+
+//     override class func introduceClass() -> String {
+//         return "A+"
+//     }
+
+//     override func speak() {
+//         super.speak()
+//         print("University student..")
+//     }
+// }
+
+// let yagom: Person = Person()
+// yagom.speak()
+
+// let jay: Student = Student()
+// jay.speak()
+
+// let jenny: UniversityStudent = UniversityStudent()
+// jenny.speak()
+
+// print(Person.introduceClass())
+// print(Student.introduceClass())
+// print(UniversityStudent.introduceClass() as String)
+// UniversityStudent.introduceClass() as Void
+
+struct BasicInformation {
+    let name: String
+    var age: Int
+}
+
+var yagomInfo: BasicInformation = BasicInformation(name: "yagom", age: 99)
+
 class Person {
-    var name: String = ""
-    var age: Int = 0
-
-    var introduction: String {
-        return "Name : \(name). Age : \(age)"
-    }
-
-    func speak() {
-        print("GaNaDa")
-    }
-
-    class func introduceClass() -> String {
-        return "Hope"
-    }
-}
-
-class Student: Person {
-    var grade: String = "F"
-
-    func study() {
-        print("Study hard...")
-    }
-
-    override func speak() {
-        print("I'm a student.")
-    }
-}
-
-class UniversityStudent: Student {
-    var major: String = ""
-
-    class func introduceClass() {
-        print(super.introduceClass())
-    }
-
-    override class func introduceClass() -> String {
-        return "A+"
-    }
-
-    override func speak() {
-        super.speak()
-        print("University student..")
-    }
+    var height: Float = 0.0
+    var weight: Float = 0.0
 }
 
 let yagom: Person = Person()
-yagom.speak()
+yagom.height = 182.5
+yagom.weight = 78.5
 
-let jay: Student = Student()
-jay.speak()
+print(yagomInfo)
+dump(yagomInfo)
 
-let jenny: UniversityStudent = UniversityStudent()
-jenny.speak()
+print(yagom)
+dump(yagom)
 
-print(Person.introduceClass())
-print(Student.introduceClass())
-print(UniversityStudent.introduceClass() as String)
-UniversityStudent.introduceClass() as Void
+// p.68
